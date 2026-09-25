77b55727452ef0232a0d38270b384ef4a7be23e8
Nguyen Phan Nhat Lan <pnhlan2005@gmail.com>
2026-09-25 22:59:25 +0700

    "change"

--- src/models/hda_v1.py
+++ src/models/hda_v1.py
@@ -12,7 +12,6 @@ class TargetAdapter(nn.Module):
             nn.Linear(input_dim, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Dropout(0.3),
         )
 
     def forward(self, x):
